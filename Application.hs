ebc68c487621427be9606c0c279aa7fc187e054f
Justin Campbell <justin@justincampbell.me>
2014-08-16 00:26:23 -0400

    Lookup synonyms from a fixed list

--- Application.hs
+++ Application.hs
@@ -1,5 +1,22 @@
-main :: IO ()
-main = putStrLn =<< contents
+import System.Environment
 
-contents :: IO String
-contents = readFile "synonyms.txt"
+data Definition = Definition { word :: String, synonyms :: [String] }
+
+main :: IO ()
+main = do
+        [input] <- getArgs
+        putStrLn $ formatOutput $ lookupDefinition input definitions
+
+formatOutput :: Maybe Definition -> String
+formatOutput (Just definition) = unlines $ synonyms definition
+formatOutput Nothing = "Not found"
+
+lookupDefinition :: String -> [Definition] -> Maybe Definition
+lookupDefinition _ [] = Nothing
+lookupDefinition w ds = if w == word this
+                            then Just this
+                            else lookupDefinition w $ tail ds
+                            where this = head ds
+
+definitions :: [Definition]
+definitions = [Definition "foo" ["bar", "baz"]]
